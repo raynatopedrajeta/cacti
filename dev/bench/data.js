bf34e26d3466e1d3d5ae0eeb70414a57c7baafe9
github-action-benchmark <github@users.noreply.github.com>
2024-05-24 08:47:59 +0000

    add Benchmark (benchmarkjs) benchmark result for e6d5d88fd33b2078035447bc0e452f3d862e0e68

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716539275638,
+  "lastUpdate": 1716540479406,
   "repoUrl": "https://github.com/raynatopedrajeta/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.33%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ruzell.vince.aquino@accenture.com",
+            "name": "ruzell22",
+            "username": "ruzell22"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "e6d5d88fd33b2078035447bc0e452f3d862e0e68",
+          "message": "feat(actionlint): fix the errors produced by the ActionLint tool\n\nPrimary Changes\n---------------\n\n1. Fix errors found by Actionlint on multiple yaml files\n2. Temporarily removed test_weaver*.yaml, weaver_deploy*.yml, weaver/ directory\nin ActionLint\n\nfixes: #2651\n\nSigned-off-by: ruzell22 <ruzell.vince.aquino@accenture.com>",
+          "timestamp": "2024-05-23T11:44:44-07:00",
+          "tree_id": "91f5f83727dd6a7b34d2ce2c7031ac542adfbed8",
+          "url": "https://github.com/raynatopedrajeta/cacti/commit/e6d5d88fd33b2078035447bc0e452f3d862e0e68"
+        },
+        "date": 1716540478366,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 730,
+            "range": "±2.65%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }

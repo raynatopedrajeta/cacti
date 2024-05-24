fc4f19922cd1f6e12b4e1b7aceac35137b0401f2
github-action-benchmark <github@users.noreply.github.com>
2024-05-24 08:27:55 +0000

    add Benchmark (benchmarkjs) benchmark result for e6d5d88fd33b2078035447bc0e452f3d862e0e68

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716539275638,
+  "repoUrl": "https://github.com/raynatopedrajeta/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ruzell.vince.aquino@accenture.com",
+            "name": "ruzell22",
+            "username": "ruzell22"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "e6d5d88fd33b2078035447bc0e452f3d862e0e68",
+          "message": "feat(actionlint): fix the errors produced by the ActionLint tool\n\nPrimary Changes\n---------------\n\n1. Fix errors found by Actionlint on multiple yaml files\n2. Temporarily removed test_weaver*.yaml, weaver_deploy*.yml, weaver/ directory\nin ActionLint\n\nfixes: #2651\n\nSigned-off-by: ruzell22 <ruzell.vince.aquino@accenture.com>",
+          "timestamp": "2024-05-23T11:44:44-07:00",
+          "tree_id": "91f5f83727dd6a7b34d2ce2c7031ac542adfbed8",
+          "url": "https://github.com/raynatopedrajeta/cacti/commit/e6d5d88fd33b2078035447bc0e452f3d862e0e68"
+        },
+        "date": 1716539273879,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 573,
+            "range": "±1.74%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 353,
+            "range": "±1.33%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      }
+    ]
+  }
+}

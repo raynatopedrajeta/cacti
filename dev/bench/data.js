1b1eb0d3c26db56153f6ba62a14dcc629f7d18e4
github-action-benchmark <github@users.noreply.github.com>
2024-08-20 07:15:16 +0000

    add Benchmark (benchmarkjs) benchmark result for ec9683d38670fe5d657b602db8215e602fd4209d

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724138115813,
+  "repoUrl": "https://github.com/raynatopedrajeta/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "adrian.batuto@accenture.com",
+            "name": "adrianbatuto",
+            "username": "adrianbatuto"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "ec9683d38670fe5d657b602db8215e602fd4209d",
+          "message": "feat(corda): support 5.1 via TS/HTTP (no JVM)\nFixes #2978\nFixes #3293\n\nSigned-off-by: adrianbatuto <adrian.batuto@accenture.com>",
+          "timestamp": "2024-08-19T14:43:10-07:00",
+          "tree_id": "530c66f1928ba9481fcc2d1d760582bf58be6677",
+          "url": "https://github.com/raynatopedrajeta/cacti/commit/ec9683d38670fe5d657b602db8215e602fd4209d"
+        },
+        "date": 1724138112065,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 595,
+            "range": "±1.61%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 353,
+            "range": "±1.91%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
